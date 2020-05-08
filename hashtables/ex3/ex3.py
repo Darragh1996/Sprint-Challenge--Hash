02d1e3c6ccc0d93823ed09b1e93866984c82f0b2
Darragh Ferry <darraghferry@hotmail.com>
2020-05-08 09:57:36 +0100

    ex3

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -1,17 +1,28 @@
 def intersection(arrays):
-
-    """
-    YOUR CODE HERE
-    """
-
-    return result
+    inter_cache = {}
+    intersection_arr = []
+    for i in range(0, len(arrays)):
+        for j in arrays[i]:
+            if j in inter_cache:
+                inter_cache[j] += 1
+            else:
+                inter_cache[j] = 1
+    for key in inter_cache:
+        if inter_cache[key] == len(arrays):
+            intersection_arr.append(key)
+    return intersection_arr
 
 
 if __name__ == "__main__":
     arrays = []
 
-    arrays.append(list(range(1000000,2000000)) + [1,2,3])
-    arrays.append(list(range(2000000,3000000)) + [1,2,3])
-    arrays.append(list(range(3000000,4000000)) + [1,2,3])
+    arrays.append(list(range(1000000, 2000000)) + [1, 2, 3])
+    arrays.append(list(range(2000000, 3000000)) + [1, 2, 3])
+    arrays.append(list(range(3000000, 4000000)) + [1, 2, 3])
 
     print(intersection(arrays))
+    # arrays2 = []
+    # arrays2.append([1, 2, 3])
+    # arrays2.append([1, 4, 5])
+    # arrays2.append([1, 6, 7])
+    # print(intersection(arrays2))
