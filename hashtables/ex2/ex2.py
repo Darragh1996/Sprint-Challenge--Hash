0ff217a050574305836a03327dd6fa7ba870f7da
Darragh Ferry <darraghferry@hotmail.com>
2020-05-08 09:22:24 +0100

    ex2

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -6,9 +6,13 @@ class Ticket:
 
 
 def reconstruct_trip(tickets, length):
-    
-    """
-    YOUR CODE HERE
-    """
-
+    tickets_cache = {}
+    for i in range(0, length):
+        tickets_cache[tickets[i].source] = tickets[i].destination
+    route = []
+    ticket_destination = tickets_cache["NONE"]
+    while ticket_destination != "NONE":
+        route.append(ticket_destination)
+        ticket_destination = tickets_cache[ticket_destination]
+    route.append("NONE")
     return route
