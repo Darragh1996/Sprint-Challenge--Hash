6aec902fe9b13fe4ab0fc4861d2e38cf14e5e60e
Darragh Ferry <darraghferry@hotmail.com>
2020-05-08 10:25:40 +0100

    ex5

--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -1,10 +1,17 @@
 def finder(files, queries):
-
-    """
-    YOUR CODE HERE
-    """
-
-    return result
+    matches = []
+    cache = {}
+    for path in files:
+        arr = path.split("/")
+        if arr[-1] in cache and path not in cache[arr[-1]]:
+            cache[arr[-1]].append(path)
+        else:
+            cache[arr[-1]] = [path]
+    for query in queries:
+        if query in cache:
+            for i in cache[query]:
+                matches.append(i)
+    return matches
 
 
 if __name__ == "__main__":
